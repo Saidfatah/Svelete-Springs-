5f6b0977c6e8d91517783b536a72d00eb4ebdc2c
ThePainter <59207693+Saidfatah@users.noreply.github.com>
2022-05-19 21:08:48 +0100

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let shown = true;
+  let moved = 0;
+
+  const toggleShow = () => (shown = !shown);
+  const toggleMove = () => (moved = moved ? 0 : 500);
+</script>
+
+<div style="opacity: {shown ? 1 : 0}">Content to toggle</div>
+<br />
+<button on:click={toggleShow}>Toggle</button>
+<hr />
+<div class="box" style="transform: translateX({moved}px)">I'm a box.</div>
+<br />
+<button on:click={toggleMove}>Move it!</button>
